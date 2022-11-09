32c5ee6ac682bbbe0331976667e924375f70a276
Kağan Kılıç <kaganklc44@gmail.com>
2022-11-09 17:43:49 +0300

    banner kısmı eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, use_key_in_widget_constructors, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
 
@@ -30,24 +30,12 @@ class MyApp extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 24.0),
-                  child: Text(
-                    'Home',
-                    style: TextStyle(
-                      fontSize: 32,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 24.0),
-                  child: Container(
-                    decoration: BoxDecoration(color: Colors.white),
-                  ),
-                )
+                //Uygulamanın Başlık Kısmı
+                buildBaslik(),
+                //Uygulamanın Banner Kısmı
+                buildBanner(),
               ],
             ),
           ),
@@ -55,4 +43,61 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget buildBaslik() {
+  return Padding(
+    padding: const EdgeInsets.only(top: 24.0),
+    child: Text(
+      'Home',
+      style: TextStyle(
+        fontSize: 32,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
+
+buildBanner() {
+  return Padding(
+    padding: EdgeInsets.only(top: 24.0),
+    child: Container(
+      padding: EdgeInsets.only(left: 24, right: 36, top: 14, bottom: 18),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(6)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Apple MacBook Air M2',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                '29.599,00 TL',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 136, 136, 136),
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500),
+              ),
+            ],
+          ),
+          Image.asset(
+            "assets/images/macbookm2.png",
+            width: 130,
+            height: 110,
+          ),
+        ],
+      ),
+    ),
+  );
 }
